6f6c74901b20c46ab8fe00755ddf26b19bbae255
Hackerokuz <higginsite_forpass@aleeas.com>
2024-07-05 13:03:05 +0200

    Added goal rendering

--- src/main/java/kaptainwutax/tungsten/mixin/MixinDebugRenderer.java
+++ src/main/java/kaptainwutax/tungsten/mixin/MixinDebugRenderer.java
@@ -2,9 +2,13 @@ package kaptainwutax.tungsten.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import kaptainwutax.tungsten.TungstenMod;
+import kaptainwutax.tungsten.render.Color;
+import kaptainwutax.tungsten.render.Cuboid;
 import net.minecraft.client.render.*;
 import net.minecraft.client.render.debug.DebugRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Vec3d;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,8 +28,19 @@ public class MixinDebugRenderer {
         BufferBuilder buffer = tessellator.getBuffer();
 
         RenderSystem.lineWidth(2.0F);
+        
+        buffer.begin(VertexFormat.DrawMode.DEBUG_LINE_STRIP, VertexFormats.POSITION_COLOR);
+        Cuboid goal = new Cuboid(TungstenMod.TARGET.subtract(0.5D, 0D, 0.5D), new Vec3d(1.0D, 2.0D, 1.0D), Color.GREEN);
+        goal.render();
+        tessellator.draw();
 
         TungstenMod.RENDERERS.forEach(r -> {
+            buffer.begin(VertexFormat.DrawMode.DEBUG_LINE_STRIP, VertexFormats.POSITION_COLOR);
+            r.render();
+            tessellator.draw();
+        });
+        
+        TungstenMod.TEST.forEach(r -> {
             buffer.begin(VertexFormat.DrawMode.DEBUG_LINE_STRIP, VertexFormats.POSITION_COLOR);
             r.render();
             tessellator.draw();
